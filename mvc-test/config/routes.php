e345eaaebfe27d48e7a94415435e035c126221cb
root <root@frodo.localdomain>
2013-07-19 16:14:38 +0300

    method recognize

--- mvc-test/config/routes.php
+++ mvc-test/config/routes.php
@@ -4,10 +4,12 @@ $cnf['admin']['namespace'] = 'Controllers\Admin1';
 
 // Administration
 $cnf['administration']['namespace'] = 'Controllers\Admin2';
-$cnf['administration']['controllers']['index'] = 'test';
-$cnf['administration']['controllers']['new'] = 'create';
+$cnf['administration']['controllers']['index']['to'] = 'test';
+$cnf['administration']['controllers']['index']['methods']['new'] = '_new';
+
+$cnf['administration']['controllers']['new']['to'] = 'create';
 
 // Defaults
 $cnf['*']['namespace'] = 'Controllers';
 
-return $cnf;
+return $cnf; 
