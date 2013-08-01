6c0b96136a2d75f8ba05182b4ca24b1d581a2578
Yavor Mihaylov <yavor.st.m@gmail.com>
2013-08-01 14:29:26 +0300

    RPC router #40

---
+++ mvc-realization/Routers/JsonRPCRouter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace \php_mvc\Routers;
+
+class JsonRPCRouter implements \php_mvc\Routers\IRouter {
+	private $map = array();
+	private $request_id = ;
+
+	public function __construct()
+	{
+		if ($_SERVER['REQUEST_METHOD'] != 'POST' OR 
+			empty($_SERVER['CONTENT_TYPE']) OR 
+			$_SERVER['CONTENT_TYPE'] != 'application/json') 
+		{
+			throw new Exception("Require json request", 400);
+			
+		}
+	}
+	
+	public function set_method_map($arr)
+	{
+		if (is_array($arr)) 
+		{
+			$this->map = $arr;
+		}
+	}
+
+	public function get_URI()
+	{
+		if ( ! is_array($this->map) OR count($this->map) == 0) 
+		{
+			$arr  = \php_mvc\App::get_instance()->get_config->rpc_routes;
+			if (is_array($arr) AND count($arr) > 0) 
+			{
+				$this->map = $arr;
+			}
+			else
+			{
+				throw new \Exception("Router require method map", 500);
+				
+			}
+		}
+
+		$request = json_decode(file_get_contents('php://input'), true);
+		if ( ! is_array($request) OR ! isset($request['method'])) 
+		{
+			throw new \Exception('Require JSON request', 400);			
+		}
+		else
+		{
+			if ($this->map[$request['method']]) 
+			{
+				$this->request_id = $request['id'];
+				return $this->map[$request['method']];
+			}
+			else
+			{
+				throw new \Exception("Method not found", 501);
+			}
+		}	
+	}
+
+	public function get_request_id()
+	{
+		return $this->request_id;
+	}
+}	
---
+++ mvc-test/config/rpc_routes.php
@@ -0,0 +1,4 @@
+<?php
+
+$cnf['get_latest_comments'] = 'post/get_coomments';
+return $cnf;
